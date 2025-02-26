1a66672852880d56dce52d3fcf0df8f59e3c0d06
ItsBreathingjet <kyle.iakovidis@gmail.com>
2025-02-25 23:26:59 -0500

    Gradle update

---
+++ android/app/src/main/java/com/lovebug/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lovebug.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lovebug.app',
+  appName: 'Lovebug',
+  webDir: 'dist'
+};
+
+export default config;
